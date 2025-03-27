32be386d9877a91f810157289d617eb5a1c6a769
Patatecosmique <pitpitbastian@gmail.com>
2025-03-27 21:36:39 +0100

    Add initial project files with weather app layout and styling

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,10 @@ button.addEventListener("click",()=>{
     let codepost = document.getElementById("code_postal").value
     alert (codepost)
 })
+
+// TEST entrainenemtn
+function rechercherMeteo() {
+    const codePostal = document.getElementById('postal-code').value;
+    const resultDiv = document.getElementById('result');
+};
+
